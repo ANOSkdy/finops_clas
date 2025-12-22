daec50771a50abd8ca60d3ec14e2f7f81482356f
KJ <lacoru43oo@gmail.com>
2025-12-22 18:06:01 +0900

    Add manual cache revalidation guidance

--- src/app/(app)/manual/page.tsx
+++ src/app/(app)/manual/page.tsx
@@ -2,6 +2,8 @@ import { ManualClient } from "@/app/(app)/manual/ManualClient";
 import type { ManualDoc } from "@/lib/manual/docs";
 import { getManualDocs } from "@/lib/manual/getManualDocs";
 
+export const revalidate = 60;
+
 export default async function ManualPage() {
   let docs: ManualDoc[] = [];
 
